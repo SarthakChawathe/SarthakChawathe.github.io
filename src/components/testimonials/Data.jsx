653e17b6fd44c1b784bd0724a0c3873913694255
Sarthak Chawathe <sarthak.chawathe2012@gmail.com>
2024-12-23 08:54:42 -0500

    Added Vartan's testimonial

--- src/components/testimonials/Data.jsx
+++ src/components/testimonials/Data.jsx
@@ -7,39 +7,47 @@ import Kapil from "../../assets/img2/Testimonials copy/kapil.jpg"
 import Wasique from "../../assets/img2/Testimonials copy/Wasique_2.jpeg"
 import Shubhada from "../../assets/img2/Testimonials copy/Shubhada.jpeg"
 import Aditi from "../../assets/img2/Testimonials copy/Aditi.jpeg"
+import Vartan from "../../assets/img2/Testimonials copy/Vartan.jpeg"
 
 
 export const Data = [
     {
         id: 1,
+        image: Vartan,
+        title: "Vartan Susmanyan",
+        position: "CEO: OneChimera",
+        description: "I had the pleasure of working with Sarthak during his time at our company as a Machine Learning Engineer and Data Analyst. He was a key part of developing our back-end chatbot and database systems, and his technical skills and problem-solving really stood out. Sarthak also helped us test text-to-image generation prompts and worked closely with the team to keep everything moving smoothly. He is proactive, communicates clearly, and was a real asset to our projects. I would happily recommend him to anyone looking for someone reliable in AI and data science.",
+    },
+    {
+        id: 2,
         image: Nasim,
         title: "Nasim Anousheh",
         position: "Professor: Indiana University Bloomington",
         description: "I have had the privilege of working with Sarthak since 2023. He has been serving as a Graduate Teaching Assistant for my graduate-level course, Data Science On-Ramp, which comprises twelve mini-courses. Sarthak's performance has been exceptional. I have no doubts that he is well-prepared to handle the many challenges that a research-based program brings forth. Sarthak Chawathe is a highly talented and dedicated individual whose expertise in Machine Learning, coupled with his exceptional teaching abilities, make him a valuable asset to any organization or educational institution.",
     },
     {
-        id: 2,
+        id: 3,
         image: Kapil,
         title: "Kapil Khandelwal",
         position: "Team Lead: Tata Consultancy Services",
         description: "Sarthak was an exceptional team member during his time with TCS, bringing immense value through his innovative system design and troubleshooting expertise. He significantly boosted our efficiency by streamlining support processes, resolving a high volume of tickets, and generating insightful reports. His proficiency in Agile methodologies, Java, SQL Server, and Microsoft Office was crucial to our success. Sarthak consistently demonstrated professionalism and dedication, leaving a lasting impact. I wish him the best of luck in his pursuit of higher education and future endeavors.",
     },
     {
-        id: 3,
+        id: 4,
         image: Wasique,
         title: "Wasique Ansari",
         position: "Colleague: Tata Consultancy Services",
         description: "During my tenure at Tata Consultancy Services, I collaborated closely with Sarthak for around 1.5 years. Throughout our time together, Sarthak consistently demonstrated exceptional professionalism, technical expertise, and a strong dedication to delivering high-quality results. He exhibited outstanding problem-solving skills and a keen eye for detail, which greatly contributed to the success of the projects at TCS. His ability to adapt to new challenges and work collaboratively within the team was invaluable. Moreover, Sarthak consistently maintained a positive attitude and displayed exemplary communication skills, making him a pleasure to work with.",
     },
     {
-        id: 4,
+        id: 5,
         image: Shubhada,
         title: "Shubhada Labde",
         position: "Professor: KJ Somaiya Institute Of Technology",
         description: "We extend our heartfelt appreciation to Mr. Sarthak Chawathe, former TCS employee and Data Analyst, and an esteemed alumnus of KJ Somaiya Institute of Technology, Mumbai. We are proud to acknowledge his exceptional contributions as a speaker in our Machine Learning & Big Data Analytics winter internship program from 6th Dec to 5th Jan 2022. Additionally, we commend his dedication as a mentor to the final year students during their project journey. Mr. Chawathe's expertise, both as a speaker and a mentor, greatly enriched the program, leaving a lasting impact on all participants.",
     },
     {
-        id: 5,
+        id: 6,
         image: Aditi,
         title: "Aditi Sonawane",
         position: "Project Partner: Indiana University Bloomington",
